e9a50a55892360b60da5883a526a11ded78c05ba
Chris Amey <callen.amey@gmail.com>
2025-04-01 18:01:02 -0400

    Added the abstract model class

---
+++ include/models/Model.h
@@ -0,0 +1,14 @@
+
+class Model {
+public:
+    virtual ~Model() = default;
+    virtual void initializeState() = 0;
+    virtual void copyStateFrom(const Model& other) = 0;
+    enum class UpdateMethod { };
+    virtual double calcEnergy() const = 0;
+    virtual void updateSweep(int numSweeps) = 0;
+    // I am not sure about creating new clones as smart pointers. I am not sure that this will be the most
+    // efficient way to deal with replicas and probably prefer to do this within a separate population class
+    // virtual std::unique_ptr<Model> clone() const = 0; 
+
+};
