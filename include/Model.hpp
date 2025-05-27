ca2b8482789ffa0f49c6074602692feaa9f67da0
Chris Amey <callen.amey@gmail.com>
2025-05-27 14:07:49 -0400

    Added the virtual base class Model.hpp.

---
+++ include/Model.hpp
@@ -0,0 +1,15 @@
+#ifndef MODEL_HPP
+#define MODEL_HPP
+
+class Model {
+public:
+    virtual ~Model() = default;
+    virtual void initializeState() = 0;
+    virtual void copyStateFrom(const Model& other) = 0;
+    
+    enum class UpdateMethod { };
+    virtual double calcEnergy() const = 0;
+    virtual void updateSweep(int numSweeps) = 0;
+};
+
+#endif // MODEL_HPP
