2635d9964375575cc7026d5e832fe774b389196f
Chris Amey <callen.amey@gmail.com>
2025-05-28 15:29:30 -0400

    Add ~IsingModel(), copyStateFrom(), calcEnergy()

--- src/models/IsingModel.cpp
+++ src/models/IsingModel.cpp
@@ -12,6 +12,38 @@ IsingModel::IsingModel(const SharedModelData<IsingModel>& shared_data)
       neighbor_table_(shared_data.neighbor_table),
       bond_table_(shared_data.bond_table) {
   spins_ = new int8_t[num_spins_];
+  for (int i = 0; i < num_spins_; ++i) {
+    spins_[i] = 1;
+  }
+}
 
-  initializeState();  // Randomize the spins or set them as per the class design
+IsingModel::~IsingModel() { delete[] spins_; }
+
+void IsingModel::initializeState(gsl_rng* r) {
+  for (int i = 0; i < num_spins_; ++i) {
+    int s = gsl_rng_uniform_int(r, 2) * 2 - 1;
+    spins_[i] = s;
+  }
+}
+
+void IsingModel::copyStateFrom(const Model& other) {
+  const IsingModel& isingOther = static_cast<const IsingModel&>(other);
+  assert(this->system_size_ == isingOther.system_size_ &&
+         "System sizes must match!");
+  assert(this->num_spins_ == isingOther.num_spins_ &&
+         "Number of spins must match!");
+  for (int i = 0; i < num_spins_; ++i) {
+    this->spins_[i] = isingOther.spins_[i];
+  }
+}
+
+double IsingModel::calcEnergy() const {
+  double energy = 0.0;
+  for (int i = 0; i < num_spins_; ++i) {
+    for (int n = 0; n < 6; n += 2) {
+      int j = neighbor_table_[i * 6 + n];
+      energy += spins_[i] * spins_[j] * bond_table_[i * 6 + n];
+    }
+  }
+  return energy / num_spins_;
 }
