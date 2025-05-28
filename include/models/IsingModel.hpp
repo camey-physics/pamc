fb9805e0797106a33e9d73c0f75a11061269675c
Chris Amey <callen.amey@gmail.com>
2025-05-28 16:00:16 -0400

    Make num_spins_, num_neighbors_, system_size_ immutable

--- include/models/IsingModel.hpp
+++ include/models/IsingModel.hpp
@@ -12,21 +12,32 @@
 
 class IsingModel : public Model {
  public:
+  // IsingModel state related methods
   explicit IsingModel(const SharedModelData<IsingModel>& shared_data);
   ~IsingModel();
   void initializeState(gsl_rng* r) override;
   void copyStateFrom(const Model& other) override;
 
+  // IsingModel specific enumerated classes
   enum class UpdateMethod { metropolis, heatBath, wolff };
+  enum class Observables { energy, magnetization };
+
+  // IsingModel observable methods
   double calcEnergy() const override;
+  double calcMagnetization() const;
+
+
   void updateSweep(int num_sweeps, double beta, gsl_rng* r) override;
 
  private:
   // Shared model data, immutable
   const int num_spins_;
+  const int num_neighbors_;
   const int system_size_;
   const int* neighbor_table_;
   const double* bond_table_;
+
+  // Owned data
   int8_t* spins_;
 };
 
