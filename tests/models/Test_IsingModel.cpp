0738a15528554832331fb40486cab37408a32851
Chris Amey <callen.amey@gmail.com>
2025-05-29 11:28:20 -0400

    Add neighbor_table test Add calcEnergy() test

--- tests/models/Test_IsingModel.cpp
+++ tests/models/Test_IsingModel.cpp
@@ -37,3 +37,50 @@ TEST(IsingModelTest, CreateNeighborTable) {
   std::vector<int> table = initializeNeighborTable3D(L);
   EXPECT_EQ(table.size(), L * L * L * 6);
 }
+
+// Test the neighbors of spin 0 and L *L
+TEST(IsingModelTest, NeighborTable) {
+  int L = 4;
+  int spin = 0;
+  int num_neighbors = 6;
+  std::vector<int> table = initializeNeighborTable3D(L);
+  std::vector<int> expectedNeighbors = {(L-1)*L*L, L*L, (L-1)*L, L, L-1, 1};
+  for (int n = 0; n < num_neighbors; ++n) {
+    EXPECT_EQ(table[spin *num_neighbors + n], expectedNeighbors[n]);
+  }
+
+  expectedNeighbors = {0, 32, 28, 20, 19, 17};
+  spin = L *L;
+    for (int n = 0; n < num_neighbors; ++n) {
+    EXPECT_EQ(table[spin *num_neighbors + n], expectedNeighbors[n]);
+  }
+
+}
+
+TEST(IsingModelTest, CalcEnergy) {
+  const int L = 5;
+  const int num_spins = L * L * L;
+  const int num_neighbors = 6;
+
+  std::vector<int> neighbor_table = initializeNeighborTable3D(L);
+  std::vector<double> bond_table(num_spins * num_neighbors, -1.0);
+
+  SharedModelData<IsingModel> data(L, num_spins, num_neighbors, neighbor_table.data(),
+                                   bond_table.data());
+
+  // Calculate energy of all spins up configuration
+  IsingModel model(data);
+  EXPECT_NEAR(model.calcEnergy(), -3.0, 1e-10);
+  // Calculate energy with one spin down
+  int ind = index3D(1, 0, 0, L);
+  model.setSpin(ind, -1);
+  EXPECT_NEAR(model.calcEnergy(), -3.0 + 12.0 /num_spins, 1e-10);
+  // Calculate energy with neighboring spin down
+  ind = index3D(0, 0, 0, L);
+  model.setSpin(ind, -1);
+  EXPECT_NEAR(model.calcEnergy(), -3.0 + 20.0 /num_spins, 1e-10);
+  // Calculate energy with second neighboring spin down
+  ind = index3D(0, L - 1, 0, L);
+  model.setSpin(ind, -1);
+  EXPECT_NEAR(model.calcEnergy(), -3.0 + 28.0 /num_spins, 1e-10);
+}
