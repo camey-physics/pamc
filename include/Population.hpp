dd2e3327b8e4adf27ae0c7391170920ea18007c0
Chris Amey <callen.amey@gmail.com>
2025-05-30 16:55:33 -0400

    Move template code implementation to header

--- include/Population.hpp
+++ include/Population.hpp
@@ -2,6 +2,7 @@
 #define POPULATION_HPP
 
 #include "Model.hpp"
+#include "SharedModelData.hpp"
 
 #include <gsl/gsl_rng.h>
 #include <vector>
@@ -9,7 +10,7 @@
 template <typename ModelType>
 class Population {
  public:
-  Population(int pop_size, const gsl_rng_type* T);
+  Population(int pop_size, const gsl_rng_type* T, const SharedModelData<ModelType>& shared_data);
   ~Population();
    
   void equilibrate(double beta, int numSweeps, typename ModelType::UpdateMethod method);
@@ -24,8 +25,34 @@ class Population {
   double beta_ = 0.0;
   int pop_size_ = 0;
   std::vector<ModelType> population_;
+  const SharedModelData<ModelType>& shared_data_;
 
   gsl_rng* r_ = nullptr;
 };
+
+#include <Population.hpp>
+
+template <typename ModelType>
+Population<ModelType>::Population(int pop_size, const gsl_rng_type* T, const SharedModelData<ModelType>& shared_data)
+    : beta_(0.0),
+      pop_size_(pop_size),
+      population_(pop_size),
+      r_(gsl_rng_alloc(T)).
+      shared_data_(shared_data) {
+  for (int i = 0; i < pop_size_; ++i) {
+    population_[i].initializeState(r_);
+  }
+}
+
+template <typename ModelType>
+Population<ModelType>::~Population() {
+  gsl_rng_free(r_);
+}
+
+template <typename ModelType>
+void Population<ModelType>::equilibrate(double beta, int numSweeps, typename ModelType::UpdateMethod method) {
+
+}
+
 
 #endif // POPULATION_HPP
