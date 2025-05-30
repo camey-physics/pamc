ac376c703648d590796053bde43d4495283bcaba
Chris Amey <callen.amey@gmail.com>
2025-05-30 08:44:34 -0400

    Add template to Population class

--- include/Population.hpp
+++ include/Population.hpp
@@ -1,11 +1,16 @@
 #ifndef POPULATION_HPP
 #define POPULATION_HPP
 
+#include "Model.hpp"
+
+#include <vector>
+
+template <typename ModelType>
 class Model {
  public:
 
  private:
-
+  std::vector<ModelType> population_;
 };
 
 #endif
