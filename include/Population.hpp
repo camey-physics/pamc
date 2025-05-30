c91f8427e3c9836fbcc5ca724340713680cb8613
Chris Amey <callen.amey@gmail.com>
2025-05-30 16:38:15 -0400

    Add Population constructor and destructor

--- include/Population.hpp
+++ include/Population.hpp
@@ -3,14 +3,29 @@
 
 #include "Model.hpp"
 
+#include <gsl/gsl_rng.h>
 #include <vector>
 
 template <typename ModelType>
-class Model {
+class Population {
  public:
+  Population(int pop_size, const gsl_rng_type* T);
+  ~Population();
+   
+  void equilibrate(double beta, int numSweeps, typename ModelType::UpdateMethod method);
+  void resample(double new_beta);
+  double suggestNextBeta();
+  double measureEnergy();
+
+  // Not enforced to be in derived models via Model.hpp, but required for Population.
+  // void measureObservable(typename ModelType::Observable);
 
  private:
+  double beta_ = 0.0;
+  int pop_size_ = 0;
   std::vector<ModelType> population_;
+
+  gsl_rng* r_ = nullptr;
 };
 
-#endif
+#endif // POPULATION_HPP
