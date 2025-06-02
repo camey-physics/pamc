c34b2f9d0c93457cea4b3731ba424dc7fcec5cf8
Chris Amey <callen.amey@gmail.com>
2025-06-02 14:16:52 -0400

    Refactor Test_Population to use a fixture-based unit test setup

--- tests/Test_Population.cpp
+++ tests/Test_Population.cpp
@@ -1,24 +1,47 @@
-#include <gsl/gsl_rng.h>
 #include <gtest/gtest.h>
-#include <math.h>
+#include <gsl/gsl_rng.h>
 
 #include "Population.hpp"
 #include "models/TestModel.hpp"
 #include "SharedModelData.hpp"
 
-TEST(PopulationTest, EquilibrateCallsUpdateSweep) {
-  SharedModelData<TestModel> dummy_data;  // could be empty
-  Population<TestModel> pop(5, gsl_rng_taus, dummy_data);
+class PopulationTestModelTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    gsl_rng_env_setup();
+    rng_ = gsl_rng_alloc(gsl_rng_taus);
 
-  pop.equilibrate(1.0, 10, TestModel::UpdateMethod::FAKE);
-
-  for (int i = 0; i < 5; ++i) {
-    EXPECT_EQ(10,  pop.getState(i));
+    pop_size = 5;
+    shared_data = SharedModelData<TestModel>{};
+    pop = std::make_unique<Population<TestModel>>(pop_size, gsl_rng_taus, shared_data);
   }
 
-  for (const auto& model : pop.getModels()) {
-    EXPECT_TRUE(model.state_initialized);
-    EXPECT_EQ(model.last_num_sweeps, 10);
-    EXPECT_EQ(model.last_beta, 1.0);
+  void TearDown() override {
+    gsl_rng_free(rng_);
+  }
+
+  int pop_size;
+  gsl_rng* rng_ = nullptr;
+  SharedModelData<TestModel> shared_data;
+  std::unique_ptr<Population<TestModel>> pop;
+};
+
+TEST_F(PopulationTestModelTest, EquilibrateWithFakeMidYieldsEnergyInExpectedRange) {
+  pop->equilibrate(1.0, 3, TestModel::UpdateMethod::FAKE_MID, rng_);
+
+  for (int i = 0; i < pop_size; ++i) {
+    double e = pop->getState(i);
+    EXPECT_GE(e, 1.0);
+    EXPECT_LT(e, 2.0);
+  }
+
+  double avg_e = pop->measureEnergy();
+  EXPECT_GE(avg_e, 1.0);
+  EXPECT_LT(avg_e, 2.0);
+
+  const auto& models = pop->getModels();
+  for (const auto& m : models) {
+    EXPECT_TRUE(m.state_initialized);
+    EXPECT_EQ(m.updates_called_, 3);
   }
 }
