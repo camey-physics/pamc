6e0022b72fc2ecff5c87177f4ea8ef3873c5c704
Chris Amey <callen.amey@gmail.com>
2025-06-02 10:25:39 -0400

    Add comments detailing duck typed methods in ModelType Add getState() and getModels() methods

--- include/Population.hpp
+++ include/Population.hpp
@@ -1,6 +1,15 @@
 #ifndef POPULATION_HPP
 #define POPULATION_HPP
 
+// Population<ModelType> assumes that ModelType defines the following methods:
+//
+//   void initializeState(gsl_rng*, const SharedModelData<ModelType>&);
+//   void updateSweep(int, UpdateMethod, double, gsl_rng*, bool [, ...]);
+//   auto getState() const;         // return type may vary
+//
+// These methods are duck-typed: they are not enforced via Model.hpp,
+// but are required for Population to compile and function correctly.
+
 #include "Model.hpp"
 #include "SharedModelData.hpp"
 
@@ -21,6 +30,12 @@ class Population {
   // Not enforced to be in derived models via Model.hpp, but required for Population.
   // void measureObservable(typename ModelType::Observable);
 
+  // population_[i].getState() is duck typed and not enforced by Model.hpp.
+  auto getState(int i) const { return population_[i].getState(); }
+
+  // Returns a const reference to the population of models for direct interaction when needed. Not intended to be used in normal circumstances; for unit testing and debugging.
+  const std::vector<ModelType>& getModels() const { return population_; }
+
  private:
   double beta_ = 0.0;
   int pop_size_ = 0;
@@ -35,10 +50,10 @@ Population<ModelType>::Population(int pop_size, const gsl_rng_type* T, const Sha
     : beta_(0.0),
       pop_size_(pop_size),
       population_(pop_size),
-      r_(gsl_rng_alloc(T)).
+      r_(gsl_rng_alloc(T)),
       shared_data_(shared_data) {
   for (int i = 0; i < pop_size_; ++i) {
-    population_[i].initializeState(r_);
+    population_[i].initializeState(r_, shared_data);
   }
 }
 
@@ -53,7 +68,7 @@ template <typename... Args>
 void Population<ModelType>::equilibrate(double beta, int num_sweeps, typename ModelType::UpdateMethod method, Args&&... extra_args) {
   beta_ = beta;
   for (int i = 0; i < pop_size_; ++i) {
-    population_[i].updateSweep(numSweeps, method, beta, std::forward<Args>(extra_args)...);
+    population_[i].updateSweep(num_sweeps, method, beta, std::forward<Args>(extra_args)...);
   }
 }
 
