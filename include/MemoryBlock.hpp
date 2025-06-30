3c10a4430d329dbe8d1c50925ef3ace3dae61b50
Chris Amey <callen.amey@gmail.com>
2025-06-30 14:18:35 -0400

    Initial commit of MemoryBlock struct

--- include/MemoryBlock.hpp
+++ include/MemoryBlock.hpp
@@ -1,7 +1,18 @@
+#ifndef MEMORY_BLOCK_HPP
+#define MEMORY_BLOCK_HPP
 
+#include <cstddef>
 #include <typeindex>
+#include <typeinfo>
 
 struct MemoryBlock {
   std::type_index type;
   std::size_t count;
+
+  template <typename T>
+  static MemoryBlock forType(std::size_t count) {
+    return { std::type_index(typeid(T)), count };
+  }
 };
+
+#endif
